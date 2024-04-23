930c7a60c4f592659a863e141343219a4e8f421d
jroper7 <josephroper57@gmail.com>
2024-04-23 04:56:09 -0400

    final push

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 // import * as MODEL from "../model/model.js"
 
-import {loadPage, mixWords } from "../model/model.js";
+import {loadPage } from "../model/model.js";
 
 
 function changeRoute() {
 let hashTag = window.location.hash;
 let pageID = hashTag.replace('#', '');
-console.log(hashTag + ' ' + pageID);
-loadPage(pageID);
+// console.log(hashTag + ' ' + pageID);
+    loadPage(pageID);
 
 
 }
@@ -20,22 +20,19 @@ function initURLListener() {
 $(document).ready(function () {
     loadPage("home")
     initURLListener();
+    //initListeners();
 });
 /**
  * The `initListeners` function adds click event listeners to all anchor elements within the
  * navigation, logging the clicked element's ID and calling the `loadPage` function with that ID.
  */
-function initListeners() {
-    $("nav a").on("click", (e) => {
-        let btnId = e.currentTarget.id
-        console.log(btnId)
+// function initListeners() {
+//     $("nav a").on("click", (e) => {
+//         let btnId = e.currentTarget.id
+//         // console.log(btnId)
 
-       loadPage(btnId)
+//        loadPage(btnId)
         
-    })
-}
+//     })
+// }
  
-$(document).ready(function () {
-    loadPage("home")
-    initListeners();
-});
--- model/model.js
+++ model/model.js
@@ -3,19 +3,19 @@
 export function loadPage(pageId) {
     if (pageId != '') {
         $.get(`pages/${pageId}.html`, function (data) {
-        console.log('data ' + data);
+        // /console.log('data ' + data);
         $('#app').html(data);
             });
         } else {
             $.get(`pages/home.html`, function (data) {
-            console.log('data ' + data);
+            // console.log('data ' + data);
             $('#app').html(data);
         });
         }
 }
 
 
-export function mixWords(firstWord, secondWord){
-    let newWord = secondWord + " " + firstWord
-    alert(newWord)
-}
+// export function mixWords(firstWord, secondWord){
+//     let newWord = secondWord + " " + firstWord
+//     alert(newWord)
+// }
